b863885aa497d983cda6af3008051146d2a58597
Elia Cunial <elia.cunial@gmail.com>
2024-12-07 22:15:43 +0100

    chore: turns out it does not make much sense to have multiple services

--- services/feature_server/src/main.rs
+++ services/feature_server/src/main.rs
@@ -1,27 +1,19 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{collections::HashMap, sync::Arc, sync::Mutex};
 
 use feature_server::FeatureStore;
 
 use actix_web::{web, App, Error, HttpResponse, HttpServer};
 use actix_web_prometheus::PrometheusMetricsBuilder;
-use parking_lot::Mutex;
 use serde::{Deserialize, Serialize};
 use tracing::{info, Level};
+use xgb_rs::{booster::Booster, dmatrix::DMatrix};
 
-#[derive(Clone, Default)]
 struct AppState {
     store: Arc<FeatureStore>,
     lock: Arc<Mutex<u8>>,
-}
-
-#[derive(Deserialize)]
-struct FeatureRequest {
-    id: String,
-}
-
-#[derive(Deserialize, Serialize)]
-struct FeatureResponse {
-    features: std::vec::Vec<f32>,
+    booster: Booster,
+    features_url: String,
+    number_of_features: usize,
 }
 
 #[derive(Deserialize, Serialize)]
@@ -29,17 +21,48 @@ struct UpdateRequest {
     path: String,
 }
 
-async fn get_feature(
-    feature_req: web::Json<FeatureRequest>,
-    app_data: web::Data<AppState>,
-) -> HttpResponse {
-    info!("Features requests for id: {}", feature_req.id);
-    let feats_id = app_data.store.get_feature(feature_req.id.as_str());
-    info!("Features requests completed for id: {}", feature_req.id);
-    match feats_id {
-        Ok(feats) => HttpResponse::Ok().json(FeatureResponse { features: feats }),
-        Err(_) => HttpResponse::NoContent().json(FeatureResponse { features: vec![] }),
+#[derive(Serialize, Deserialize)]
+struct ScoringRequest {
+    #[serde(rename = "f1")]
+    id: String,
+    #[serde(rename = "f2")]
+    num_var: f32,
+}
+#[derive(Serialize, Deserialize)]
+struct ScoringResponse {
+    score: f32,
+}
+
+async fn score(req: web::Json<ScoringRequest>, app_data: web::Data<AppState>) -> HttpResponse {
+    info!("Feature Request {}", req.id.as_str());
+    let feats_id = app_data.store.get_feature(req.id.as_str());
+    let features = match feats_id {
+        Ok(feats) => feats,
+        Err(_) => return HttpResponse::NoContent().body("No features found"),
+    };
+    let booster = &app_data.booster;
+    if features.len() != app_data.number_of_features {
+        return HttpResponse::NotFound().body(format!(
+            "Id {} does not have right amount of features",
+            req.id.as_str()
+        ));
     }
+    let dmat = DMatrix::try_from_data(
+        features.as_ref(),
+        1,
+        features.len() as u64,
+    )
+    .expect("Cannot create dmatrix");
+    let predict = booster.predict(&dmat);
+    let vec = predict.expect("Cannot compute score");
+    let score = vec.first().unwrap();
+    info!(
+        "Computed score {} for id {} with features {}",
+        score,
+        req.id.as_str(),
+        serde_json::to_string(&features).unwrap()
+    );
+    HttpResponse::Ok().json(ScoringResponse { score: *score })
 }
 
 async fn batch_update(
@@ -65,21 +88,37 @@ async fn main() -> std::io::Result<()> {
     let num_workers: usize = std::env::var("FEATURES_WORKERS")
         .unwrap_or("2".to_string())
         .parse()
-        .expect("Cannote converto num workers to usize");
+        .expect("Cannot converto num workers to usize");
     tracing_subscriber::fmt()
         .with_max_level(Level::DEBUG)
         .with_target(false)
         .init();
-    let shared_data = web::Data::new(AppState::default());
     let prom = PrometheusMetricsBuilder::new("api")
         .endpoint("/metrics")
         .build()
         .unwrap();
+    let feature_store = Arc::new(FeatureStore::default());
+    let lock = Arc::new(Mutex::new(0));
     HttpServer::new(move || {
+        let features_url = String::default();
+        let booster = Booster::new().expect("Cannot load model");
+        booster
+            .load_model("assets/silly_model.json")
+            .expect("Cannot load model");
+        let number_of_features = booster
+            .get_number_of_features()
+            .expect("Cannot extract models num feats");
+        let shared_data = web::Data::new(AppState {
+            store: feature_store.clone(),
+            lock: lock.clone(),
+            booster,
+            features_url,
+            number_of_features,
+        });
         App::new()
             .app_data(shared_data.clone())
             .wrap(prom.clone())
-            .route("/feature", web::to(get_feature))
+            .route("/score", web::to(score))
             .route("/batch_update", web::to(batch_update))
     })
     .bind(format!("{}:{}", host.as_str(), port.as_str()))?
