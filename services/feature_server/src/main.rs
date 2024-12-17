d0f37ebfff2031ccd9a2e25d96a8a5397b94c7db
Elia Cunial <elia.cunial@gmail.com>
2024-12-18 00:40:41 +0100

    simplift main

--- services/feature_server/src/main.rs
+++ services/feature_server/src/main.rs
@@ -1,20 +1,12 @@
-use std::{
-    collections::HashMap,
-    io::Write,
-    sync::{Arc, Mutex},
-};
+use std::io::Write;
 
-use feature_server::FeatureStore;
-
-use actix_web::{web, App, Error, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web_prometheus::PrometheusMetricsBuilder;
 use serde::{Deserialize, Serialize};
-use tracing::{info, Level};
+use tracing::{debug, info, warn, Level};
 use xgb_rs::{booster::Booster, dmatrix::DMatrix};
 
 struct AppState {
-    store: Arc<FeatureStore>,
-    lock: Arc<Mutex<u8>>,
     booster: Booster,
     number_of_features: usize,
     features_path: String,
@@ -49,29 +41,37 @@ struct CreateRequest {
 }
 
 async fn score(req: web::Json<ScoringRequest>, app_data: web::Data<AppState>) -> HttpResponse {
-    info!("Feature Request {}", req.id.as_str());
+    debug!("Feature Request {}", req.id.as_str());
+    let start = std::time::Instant::now();
     let features_file = format!(
         "{}/{}.json",
         app_data.features_path.as_str(),
         req.id.as_str()
     );
-    info!("Getting features from {}", features_file.as_str());
+    debug!("Getting features from {}", features_file.as_str());
     let feats_str = match std::fs::read_to_string(features_file.as_str()) {
         Ok(feats_str) => feats_str,
-        Err(_) => {
-            info!("Could not read file {}", features_file.as_str());
+        Err(e) => {
+            debug!("Could not read file {}, {}", features_file.as_str(), e);
+            let duration = start.elapsed().as_millis();
+            info!(duration=duration, response_code = 204);
             return HttpResponse::NoContent().body("Cannot get features");
         }
     };
-    info!("Get features {}", req.id.as_str());
+    debug!("Get features {}", req.id.as_str());
     let id_features: IdFeatures = match serde_json::from_str(feats_str.as_str()) {
         Ok(idf) => idf,
-        Err(_) => return HttpResponse::NotAcceptable().body("Cannot deserialize feats"),
+        Err(e) => {
+            warn!("Cannot deserialize file {}, {}", features_file.as_str(), e);
+            let duration = start.elapsed().as_millis();
+            info!(duration=duration, response_code = 406);
+            return HttpResponse::NotAcceptable().body("Cannot deserialize feats");
+        }
     };
     let features = id_features.features;
     let booster = &app_data.booster;
     if features.len() != app_data.number_of_features {
-        return HttpResponse::NotFound().body(format!(
+        return HttpResponse::BadRequest().body(format!(
             "Id {} does not have right amount of features",
             req.id.as_str()
         ));
@@ -81,36 +81,22 @@ async fn score(req: web::Json<ScoringRequest>, app_data: web::Data<AppState>) ->
     let predict = booster.predict(&dmat);
     let vec = predict.expect("Cannot compute score");
     let score = vec.first().unwrap();
-    info!(
+    debug!(
         "Computed score {} for id {} with features {}",
         score,
         req.id.as_str(),
         serde_json::to_string(&features).unwrap()
     );
+    let duration = start.elapsed().as_millis();
+    info!(duration=duration, response_code = 200);
     HttpResponse::Ok().json(ScoringResponse { score: *score })
-}
-
-async fn batch_update(
-    update_req: web::Json<UpdateRequest>,
-    app_data: web::Data<AppState>,
-) -> Result<HttpResponse, Error> {
-    let _lock = app_data.lock.lock();
-    info!("Updating features from path: {}", update_req.path);
-    let file_content = std::fs::read_to_string(update_req.path.as_str()).map_err(|e| {
-        actix_web::error::ErrorBadRequest(format!("Cannot read json at {}: {}", update_req.path, e))
-    })?;
-    let batch_update: HashMap<String, Vec<f32>> = serde_json::from_str(file_content.as_str())
-        .map_err(|e| actix_web::error::ErrorBadRequest(format!("Ill formed json: {}", e)))?;
-    app_data.store.batch_update_features(batch_update);
-    info!("Updated features from path: {}", update_req.path);
-    Ok(HttpResponse::Ok().into())
 }
 
 async fn create_feature(
     create_req: web::Json<CreateRequest>,
     app_data: web::Data<AppState>,
 ) -> HttpResponse {
-    info!("Creating feature {}", create_req.id.as_str());
+    debug!("Creating feature {}", create_req.id.as_str());
     if create_req.features.len() != app_data.number_of_features {
         return HttpResponse::BadRequest().body("Not enough features");
     }
@@ -135,7 +121,7 @@ async fn create_feature(
     };
     match writer.flush() {
         Ok(_) => {
-            info!("Created feature {}", create_req.id.as_str());
+            debug!("Created feature {}", create_req.id.as_str());
             HttpResponse::Ok().into()
         }
         Err(_) => HttpResponse::BadRequest().body("Cannot write file"),
@@ -175,8 +161,6 @@ async fn main() -> std::io::Result<()> {
         .endpoint("/metrics")
         .build()
         .unwrap();
-    let feature_store = Arc::new(FeatureStore::default());
-    let lock = Arc::new(Mutex::new(0));
     HttpServer::new(move || {
         let booster = Booster::new().expect("Cannot load model");
         booster
@@ -186,8 +170,6 @@ async fn main() -> std::io::Result<()> {
             .get_number_of_features()
             .expect("Cannot extract models num feats");
         let shared_data = web::Data::new(AppState {
-            store: feature_store.clone(),
-            lock: lock.clone(),
             booster,
             number_of_features,
             features_path: if let Ok(p) = std::env::var("FEATURES_PATH") {
@@ -200,7 +182,6 @@ async fn main() -> std::io::Result<()> {
             .app_data(shared_data.clone())
             .wrap(prom.clone())
             .route("/score", web::to(score))
-            .route("/batch_update", web::to(batch_update))
             .route("/create_feature", web::to(create_feature))
             .route("/health", web::to(get_health_check))
     })
