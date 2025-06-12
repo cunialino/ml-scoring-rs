5ae47b8b76615f416b69334455b673952c0eb114
Elia Cunial <elia.cunial@gmail.com>
2025-06-12 18:36:38 +0200

    feat(scoring-rs): send metrics to prom and timeout after 31 ms

--- services/scoring_server/src/main.rs
+++ services/scoring_server/src/main.rs
@@ -1,7 +1,10 @@
 use actix_web::{web, App, HttpResponse, HttpServer, ResponseError};
-use actix_web_prometheus::PrometheusMetricsBuilder;
+use actix_web_prom::PrometheusMetricsBuilder;
+use prometheus::{Histogram, HistogramOpts};
 use thiserror::Error;
+use tokio::time::{timeout, Duration};
 use tracing::{debug, info, Level};
+use tracing_actix_web::TracingLogger;
 use xgb_rs::{booster::Booster, dmatrix::DMatrix};
 
 #[derive(Debug, Error)]
@@ -19,6 +22,8 @@ pub enum AppError {
     XGBErr(#[from] xgb_rs::booster::XGBoostError),
     #[error("DMatrix Error {0}")]
     DMATErr(#[from] xgb_rs::dmatrix::DMatrixError),
+    #[error("Request timed out")]
+    Timeout,
 }
 
 impl ResponseError for AppError {
@@ -42,15 +47,14 @@ impl ResponseError for AppError {
                 "Id {} does not have the right amount of features",
                 id
             )),
-            AppError::EmptyScore => {
-                HttpResponse::InternalServerError().body("Empty score")
-            }
+            AppError::EmptyScore => HttpResponse::InternalServerError().body("Empty score"),
             AppError::XGBErr(e) => {
                 HttpResponse::InternalServerError().body(format!("XGBoost Error: {}", e))
             }
             AppError::DMATErr(e) => {
                 HttpResponse::InternalServerError().body(format!("DMatrix Error: {}", e))
             }
+            AppError::Timeout => HttpResponse::RequestTimeout().body("Request timeout"),
         }
     }
 }
@@ -61,31 +65,52 @@ struct AppState {
     rocksdb: std::sync::Arc<kv_store::KvStore<kv_store::Secondary>>,
 }
 
+struct PromHist {
+    xgboost_hist: Histogram,
+    rocksdb_hist: Histogram,
+}
+
 async fn score(
     req: web::Json<common::ScoringRequest>,
+    prom_hist: web::Data<PromHist>,
     app_data: web::Data<AppState>,
 ) -> Result<HttpResponse, AppError> {
-    debug!("Feature Request {}", req.id.as_str());
-    let start = std::time::Instant::now();
+    let response_future = async {
+        debug!("Feature Request {}", req.id.as_str());
+        let start = std::time::Instant::now();
 
-    let rb = app_data.rocksdb.clone();
-    let id = req.id.clone();
+        let rb = app_data.rocksdb.clone();
+        let id = req.id.clone();
 
-    let ff = web::block(move || rb.get_feature(&id)).await??;
+        let timer1 = prom_hist.rocksdb_hist.start_timer();
+        let ff = web::block(move || rb.get_feature(&id)).await??;
+        timer1.observe_duration();
 
-    let features = ff.features;
-    let booster = app_data.booster.clone();
-    if features.len() != app_data.number_of_features {
-        return Err(AppError::WrongFeaturesCount(req.id.clone()));
-    }
-    let score = web::block(move || -> Result<f32, AppError> {
-        let dmat = DMatrix::try_from_data(features.as_ref(), 1, features.len() as u64)?;
-        let predict = booster.predict(&dmat)?;
-        Ok(predict.first().ok_or(AppError::EmptyScore)?.to_owned())
-    })
-    .await??;
-    let duration = start.elapsed().as_millis();
-    info!(feature_id = req.id, duration = duration, response_code = 200);
+        let features = ff.features;
+        let booster = app_data.booster.clone();
+        if features.len() != app_data.number_of_features {
+            return Err(AppError::WrongFeaturesCount(req.id.clone()));
+        }
+
+        let timer2 = prom_hist.xgboost_hist.start_timer();
+        let score = web::block(move || -> Result<f32, AppError> {
+            let dmat = DMatrix::try_from_data(features.as_ref(), 1, features.len() as u64)?;
+            let predict = booster.predict(&dmat)?;
+            Ok(predict.first().ok_or(AppError::EmptyScore)?.to_owned())
+        })
+        .await??;
+        timer2.observe_duration();
+        let duration = start.elapsed().as_millis();
+        info!(
+            feature_id = req.id,
+            duration = duration,
+            response_code = 200
+        );
+        Ok(score)
+    };
+    let score = timeout(Duration::from_millis(31), response_future)
+        .await
+        .map_err(|_| AppError::Timeout)??;
     Ok(HttpResponse::Ok().json(common::ScoringResponse { score }))
 }
 
@@ -120,15 +145,37 @@ async fn main() -> std::io::Result<()> {
             .init();
     } else {
         tracing_subscriber::fmt()
-            .with_max_level(Level::DEBUG)
+            .with_max_level(Level::ERROR)
             .with_target(false)
             .init();
     }
 
-    let prom = PrometheusMetricsBuilder::new("api")
+    let prometheus = PrometheusMetricsBuilder::new("api_score")
         .endpoint("/metrics")
         .build()
         .unwrap();
+
+    let registry = prometheus.registry.clone();
+
+    let rocksdb_opts = HistogramOpts::new(
+        "api_score_rocksdb_duration_seconds",
+        "Latency of RocksDB lookup in /score endpoint",
+    );
+    let rocksdb_hist = Histogram::with_opts(rocksdb_opts).unwrap();
+    registry.register(Box::new(rocksdb_hist.clone())).unwrap();
+
+    let xgb_opts = HistogramOpts::new(
+        "api_score_xbg_duration_seconds",
+        "Latency of XGBoost predict in /score endpoint",
+    );
+    let xgb_hist = Histogram::with_opts(xgb_opts).unwrap();
+    registry.register(Box::new(xgb_hist.clone())).unwrap();
+    let prom_hists = PromHist {
+        rocksdb_hist,
+        xgboost_hist: xgb_hist,
+    };
+    let prom_hists_data = web::Data::new(prom_hists);
+
     // Optimize for read-heavy workload on network storage
     let cache_size = 8 * 1024 * 1024; // 512MB
 
@@ -161,7 +208,9 @@ async fn main() -> std::io::Result<()> {
         });
         App::new()
             .app_data(shared_data.clone())
-            .wrap(prom.clone())
+            .app_data(prom_hists_data.clone())
+            .wrap(TracingLogger::default())
+            .wrap(prometheus.clone())
             .route("/score", web::to(score))
             .route("/health", web::to(get_health_check))
             .route("/update_features", web::to(update_features))
