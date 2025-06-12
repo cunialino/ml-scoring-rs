b086636bc1af429f937850153ad1480331871bb8
Elia Cunial <elia.cunial@gmail.com>
2025-06-12 18:35:53 +0200

    feat(update-features): write sync and notify more than once

--- services/update_features/src/main.rs
+++ services/update_features/src/main.rs
@@ -1,11 +1,13 @@
-use anyhow::Ok;
-use futures::future::join_all;
+use k8s_openapi::api::core::v1::{Pod, Service};
+use kube::{
+    api::{Api, ListParams},
+    Client,
+};
 use rand::Rng;
+use reqwest::Client as HttpClient;
 use std::sync::Arc;
 use std::time::Instant;
-use kube::{Client, api::{Api, ListParams}};
-use k8s_openapi::api::core::v1::{Pod, Service};
-use reqwest::Client as HttpClient;
+use tracing::info;
 
 const N_IDS: usize = 15_000_000;
 const N_COLS: usize = 30;
@@ -68,7 +70,7 @@ async fn main() -> anyhow::Result<()> {
             .init();
     } else {
         tracing_subscriber::fmt()
-            .with_max_level(tracing::Level::DEBUG)
+            .with_max_level(tracing::Level::INFO)
             .with_target(false)
             .init();
     }
@@ -78,56 +80,51 @@ async fn main() -> anyhow::Result<()> {
     let kv = kv_store::KvStore::try_new_primary(&db_path, 8 * 1024 * 1024).unwrap();
     let db = Arc::new(kv);
 
-    // Concurrency limiter
-    let semaphore = Arc::new(tokio::sync::Semaphore::new(10));
-
     // Partition IDs into batches
-    let mut batch_tasks = vec![];
     for chunk in (0..N_IDS).collect::<Vec<_>>().chunks(BATCH_SIZE) {
+        info!("Writing batch");
         let slice = chunk.to_vec();
         let db_clone = db.clone();
-        let sem = semaphore.clone();
-        // Spawn a task per batch
-        let fut = tokio::spawn(async move {
-            let _permit = sem.acquire().await.unwrap();
-            let batch_start = Instant::now();
+        let batch_start = Instant::now();
 
-            let mut my_stuff = Vec::with_capacity(slice.len());
-            let mut keys = Vec::with_capacity(slice.len());
-            for &i in &slice {
-                // generate features
-                let features: Vec<f32> = (0..N_COLS)
-                    .map(|_| rand::thread_rng().gen::<f32>() * 100.)
-                    .collect();
+        let mut my_stuff = Vec::with_capacity(slice.len());
+        let mut keys = Vec::with_capacity(slice.len());
+        for &i in &slice {
+            // generate features
+            let features: Vec<f32> = (0..N_COLS)
+                .map(|_| rand::thread_rng().gen::<f32>() * 100.)
+                .collect();
 
-                keys.push(format!("feature_{}", i));
+            keys.push(format!("feature_{}", i));
 
-                // SAFETY: Because we reserved capacity, `keys` will never move
-                //         or reallocate, so this raw pointer stays valid.
-                let last_ptr: *const String = keys.last().unwrap();
-                let key_str: &str = unsafe { (*last_ptr).as_str() };
-                my_stuff.push((key_str, common::IdFeatures { features }));
+            // SAFETY: Because we reserved capacity, `keys` will never move
+            //         or reallocate, so this raw pointer stays valid.
+            let last_ptr: *const String = keys.last().unwrap();
+            let key_str: &str = unsafe { (*last_ptr).as_str() };
+            my_stuff.push((key_str, common::IdFeatures { features }));
+        }
+        db_clone.write_features(&my_stuff).unwrap();
+
+        db.finalize_writes();
+
+        tokio::spawn(async {
+            match update_features_all_pods().await {
+                Ok(_) => info!("Updated all features"),
+                Err(e) => info!("Err updating pods: {:?}", e),
             }
-            db_clone.write_features(&my_stuff).unwrap();
-
-            let duration = batch_start.elapsed();
-            println!(
-                "Batch of {} features written in {:?}",
-                slice.len(),
-                duration
-            );
         });
-        batch_tasks.push(fut);
+
+        println!("Updating pods");
+        let duration = batch_start.elapsed();
+        println!(
+            "Batch of {} features written in {:?}",
+            slice.len(),
+            duration
+        );
     }
 
-    // Wait for all batches
-    let _ = join_all(batch_tasks).await;
-
-    db.finalize_writes();
     let total_duration = start_time.elapsed();
     println!("All {} features written in {:?}", N_IDS, total_duration);
-    println!("Updating pods");
-    update_features_all_pods().await?;
     println!("Ending at: {:?}", Instant::now());
     Ok(())
 }
